a7c18b1e8945bbabf5ec0ab3a1fec2353a0d5367
Moreno Maroccia <marocciamoreno@gmail.com>
2023-04-13 13:32:32 +0200

    esercizio10

--- es-28/exercise-2.js
+++ es-28/exercise-2.js
@@ -1,5 +1,15 @@
-const order = {};
+const order = {
+  customer:{
+    address:{
+     city: 'Roma'
 
-if (order && order.customer && order.customer.address && !order.customer.address.city) {
+    }
+  }
+};
+
+
+if(!order?.customer?.address?.city){
   console.log('City is required');
-}
+}else{
+  console.log('correct')
+};
