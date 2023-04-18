813d6cdab247a490c780bc58b25a17d4374af1b9
Moreno Maroccia <marocciamoreno@gmail.com>
2023-04-18 15:32:30 +0200

    esercizio21

---
+++ es-60/exercise-1.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 20;
+console.log('Before swap: ', num1, num2); // Before swap: 10 20
+
+
+
+[num3, num4] = [10, 20];
+num1 = num4;
+num2 = num3;
+
+console.log('After swap: ', num1, num2); // After swap: 20 10
